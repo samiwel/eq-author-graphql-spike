60856bb4084204bc9daa8d5d24377fb8beaab3f4
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-03-14 17:30:49 +0000

    Added a few more query endpoints.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import graphene
+import json
 import os
 
 from flask import Flask, render_template, url_for
 from flask_foundation import Foundation
 from flask_graphql import GraphQLView
+from graphene.types.json import JSONString
 
 app = Flask(__name__)
 
@@ -13,18 +15,33 @@ app.config['FOUNDATION_USE_MINIFIED'] = True
 app.config['FOUNDATION_USE_CDN'] = True
 app.config['SECRET_KEY'] = 'devkey'
 
-class Query(graphene.ObjectType):
-    hello = graphene.String()
+schema_json = None
+with open('data/schema.json') as schema_file:
+    schema_json = json.load(schema_file)
+    schema_file.close()
 
-    def resolve_hello(self, args, context, info):
-        return 'World'
+class Query(graphene.ObjectType):
+    schema = JSONString()
+    groups = JSONString()
+    blocks = JSONString()
+    questions = JSONString()
+
+    def resolve_schema(self, args, context, info):
+        return schema_json
+
+    def resolve_groups(self, args, context, info):
+        return schema_json['groups']
+
+    def resolve_blocks(self, args, context, info):
+        return schema_json['groups'][0]['blocks']
+
+    def resolve_questions(self, args, context, info):
+        return schema_json['groups'][0]['blocks'][0]['sections'][0]['questions']
 
 schema = graphene.Schema(query=Query)
-
-
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql',
                                                            schema=schema,
-                                                           graphiql=True,
+                                                           graphiql=os.environ.get('DEBUG', False),
                                                            pretty=True))
 
 @app.route('/')
